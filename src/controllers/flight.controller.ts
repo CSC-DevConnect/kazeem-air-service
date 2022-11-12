ee836594c2db39cc794fa7a52f51a18de8e4e6da
olashina201 <olashina201@gmail.com>
2022-11-12 23:41:07 +0100

    update codebase

--- src/controllers/flight.controller.ts
+++ src/controllers/flight.controller.ts
@@ -8,16 +8,6 @@ import { flightService } from '../services';
 | Get all airlines
 |------------------------------------------------------------------------
 */
-// const bookFlight = catchAsync(async (req: Request, res: Response) => {
-//   const flight = await flightService.bookFlight(req.body);
-//   res.status(httpStatus.CREATED).send({ flight });
-// });
-
-/*
-|-----------------------------------------------------------------------
-| Get all airlines
-|------------------------------------------------------------------------
-*/
 const createOfferRequest = catchAsync(async (req: Request, res: Response) => {
   const flight = await flightService.createOfferRequest(req.body);
   res.status(httpStatus.CREATED).send({ flight });
