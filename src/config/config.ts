314a695438563a6fb94b32056f3af7aa219587ee
olashina201 <olashina201@gmail.com>
2022-11-18 21:43:30 +0100

    update codebase

--- src/config/config.ts
+++ src/config/config.ts
@@ -36,7 +36,7 @@ export default {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+    url: process.env.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,
